eb1d83e9adfcd4f083313a0a8ad764b0207a5833
github-dns147 <rarden@ya.ru>
2021-10-16 18:58:32 +0300

    feat: add move slider (Sat, Oct 16, 2021 06:58:31 PM)

--- cssMemSlider/index.js
+++ cssMemSlider/index.js
@@ -1,8 +1,15 @@
 const sliderContainer = document.querySelector('.picture');
+const sliderContainerUp = document.querySelector('.picture__up');
 const sub = document.querySelector('.sub');
 const sliderPags = document.querySelectorAll('.slider__pag__li');
 
-console.log(sliderPags);
+const subList = {
+    1: "Everything! I'm going to sleep)",
+    2: "When your salary was raised)",
+    3: "I'm watching you!",
+    4: "Please! Please! Please!",
+    5: "I managed to do it before the deadline!",
+};
 
 sliderPags[0].classList.add('slider__pags__active');
 
@@ -28,33 +35,25 @@ sliderPags.forEach((v, i) => {
     });
 
     v.addEventListener('mousedown', () => {
-        const activePag = document.querySelector('.slider__pags__active__hover');
-
         sliderPags.forEach((v) => v.classList.remove('slider__pags__active'));
-
-        //if (v !== activePag) {
-            v.classList.add('slider__pags__active');
-        //}
-
+        v.classList.add('slider__pags__active');
         v.classList.add('slider__pags__active__hover');
 
-        // if (i > countSlide) {
-        //     moveLeft();
-        // }
-
-        // if (i < countSlide) {
-        //     moveRight();
-        // }
-
-        // countSlide = i;
-        // sliderPags.forEach((v) => v.style.backgroundColor = '#ffffff');
-        // v.style.backgroundColor = '#9d8665';
-
-        // sliderNum.innerHTML = '0' + (countSlide + 1);
-        // welcomContainer.style.setProperty('--welcom-img', `url("assets/img/welcome-slider/${countSlide + 1}.jpg")`);
+        sliderContainer.style.setProperty('--img', `url("galery/${i}.gif")`);
     });
 
     v.addEventListener('mouseup', () => {
         v.classList.remove('slider__pags__active__hover');
+
+        sub.innerHTML = subList[i + 1];
+        sliderContainerUp.style.setProperty('--img', `url("galery/${i + 1}.gif")`);
+
+        sliderContainerUp.animate(
+            [{backgroundPositionX: '1024px'}, {backgroundPositionX: '0px'}],  
+            {
+            duration: 400,
+            easing: 'ease-out',
+            }
+        );
     });
 });
